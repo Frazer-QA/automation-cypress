38938cf7b289764aabbc8e217886552c27a38c15
Frazer Shaw <>
2020-03-04 17:23:42 +0000

    Adding auth tests

---
+++ cypress/integration/test/test1.js
@@ -0,0 +1,56 @@
+describe("Actions", () => {
+    beforeEach(() => {
+      cy.visit("/login") //Specify extra URL folder structure if needed between apostrophes 
+    })
+  
+    it('Login - Unauthorised', () => {
+
+        cy.get('#username')
+        .type('testuser')
+
+        cy.get('#password')
+        .type('testPassword')
+
+        cy.get("[class=radius][type=submit]")
+        .click()
+
+        cy.get("#flash")
+        .should("be.visible")
+
+    })
+
+    it('Login - Authorised', () => {
+
+        cy.get('#username')
+        .type('tomsmith')
+
+        cy.get('#password')
+        .type('SuperSecretPassword!')
+
+        cy.get("[class=radius][type=submit]")
+        .click()
+
+        cy.url()
+        .should('eq', 'http://the-internet.herokuapp.com/secure')
+
+    })
+
+    it('Login - Authorised - Logout', () => {
+
+        cy.get('#username')
+        .type('tomsmith')
+
+        cy.get('#password')
+        .type('SuperSecretPassword!')
+
+        cy.get("[class=radius][type=submit]")
+        .click()
+
+        cy.url()
+        .should('eq', 'http://the-internet.herokuapp.com/secure')
+
+    })
+
+})
+
+    
